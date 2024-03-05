31a01b6bb3d5795b5e74abcb5ce39fe5f1290aae
Camilo Delgado <camilodelgado0305@gmail.com>
2024-03-05 18:42:05 -0500

    proxy2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,12 @@ const URL= import.meta.env.VITE_BACKEND_URL || "http://localhost:8080";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/socket.io": {
+        target: URL,
+        ws: true,
+      },
+    },
+  },
 });
