fa724bfc0e3319dde78c7f6a1d8cfec6f56db3ad
Camilo Delgado <camilodelgado0305@gmail.com>
2024-03-05 20:43:48 -0500

    config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import "dotenv/config"
 
+const URL= process.env.VITE_BACKEND_URL || "http://localhost:8080";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/socket.io": {
-        target: process.env.VITE_BACKEND_URL,
+        target: URL,
         ws: true,
       },
     },
